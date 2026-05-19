2a83fd43bdb593006184b1c12153bb9a5244ed17
SacralSalmon <vendettasoonfeed@gmail.com>
2026-05-19 18:15:40 +0300

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,18 @@
+const movies = [
+  {
+    answers: [
+      "interstellar",
+      "интерстеллар"
+    ],
+
+    displayTitle: "Интерстеллар",
+
+    frames: [
+      "images/interstellar-1.jpeg",
+      "images/interstellar-2.jpeg",
+      "images/interstellar-3.jpeg",
+      "images/interstellar-4.jpeg",
+      "images/interstellar-5.jpeg"
+    ]
+  }
+];
