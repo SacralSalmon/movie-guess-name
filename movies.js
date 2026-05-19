9d2d44a65bc6ba61fa623c9e08549c8763021c28
SacralSalmon <vendettasoonfeed@gmail.com>
2026-05-19 17:32:29 +0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -8,11 +8,11 @@ const movies = [
     displayTitle: "Интерстеллар",
 
     frames: [
-      "images/interstellar-1.png",
-      "images/interstellar-2.png",
-      "images/interstellar-3.png",
-      "images/interstellar-4.png",
-      "images/interstellar-5.png"
+      "images/interstellar-1.jpeg",
+      "images/interstellar-2.jpeg",
+      "images/interstellar-3.jpeg",
+      "images/interstellar-4.jpeg",
+      "images/interstellar-5.jpeg"
     ]
   }
 ];
